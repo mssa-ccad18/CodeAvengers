3f87c5a098c859ff800b83baf241c89b9c507131
Chris Gosseen <185705028+cgosseen@users.noreply.github.com>
2025-05-10 14:07:13 -0400

    mid-saturday update: fixed boolean logic for redirect to LandingPage upon successful account creation consolidated and added error prompting for account creation:           errors accounted for: username already taken, username/password/email fields empty, password mismatch with verifyPassword field

--- Projects/CodeChat/CodeChat/CodeChat/Hubs/ChatHub.cs
+++ Projects/CodeChat/CodeChat/CodeChat/Hubs/ChatHub.cs
@@ -22,26 +22,48 @@ public class ChatHub : Hub
         await Clients.All.SendAsync("ReceiveMessage", user, message, date);
     }
 
-    public async Task CreateUser(string username, string password, string email) {
+    public async Task<bool> CreateUser(string username,string email, string password, string verifyPassword) {
 
         try {
 
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password)) {
-                throw new HubException("Username and Public Key are required");
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email)) {
+
+                await Clients.Caller.SendAsync("AccountCreationFailed", "Username, password, and email are required.");
+                return false;
             }
 
+            //userName parameters check
+
             if (await _db.Users.AnyAsync(u => u.Username == username)) {
-                throw new HubException($"Username '{username}' is already taken");
+
+                await Clients.Caller.SendAsync("AccountCreationFailed", $"Username '{username}' is already taken.");
+                return false;
+            }
+
+            //email already in dB
+
+            //email invalid format 
+
+            //password parameter check 
+
+            if(password != verifyPassword)
+            {
+                await Clients.Caller.SendAsync("AccountCreationFailed", "Passwords do not match");
+                return false;
             }
 
             var user = new User { Username = username, Password = password, Email = email };
 
             _db.Users.Add(user);
             await _db.SaveChangesAsync();
+            await Clients.Caller.SendAsync("AccountCreationSuccess", "Account created successfully! Redirecting to Homepage for login...");
 
-            await Clients.All.SendAsync("UserCreated", user);
-            } catch (Exception ex) {
-            throw new HubException($"Error creating user: {ex.Message}");
+            //await Clients.All.SendAsync("UserCreated", user);
+            return true;
+            } 
+        catch (Exception ex) {
+            await Clients.Caller.SendAsync("AccountCreationFailed", $"Error creating user: {ex.Message}");
+            return false;
         }
 
     }
